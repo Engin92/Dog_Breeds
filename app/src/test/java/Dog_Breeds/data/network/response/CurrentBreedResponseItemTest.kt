b8dc3d314aaf7547d1c36472c756fa97269cc90c
Engin92 <26438895+Engin92@users.noreply.github.com>
2020-05-25 22:37:11 +0200

    - added: unit Tests > for class CurrentBreedResponseItem > for class Height (However, the data read from the Internet using the dog.API does not always have the correct metric/imperial ratio)

---
+++ app/src/test/java/Dog_Breeds/data/network/response/CurrentBreedResponseItemTest.kt
@@ -0,0 +1,89 @@
+package Dog_Breeds.data.network.response
+
+import junit.framework.Assert.*
+import org.junit.Assert
+import org.junit.Test
+
+
+internal class CurrentBreedResponseItemTest {
+
+    val NAME_1 = "Affenpinscher"
+    val ID_1 = 1
+    val BRED_FOR_1 = "Small rodent hunting, lapdog"
+    val TEMPERAMENT_1 = "Stubborn, Curious, Playful, Adventurous, Active, Fun-loving"
+    val LIFE_SPAN_1 = "10 - 12 years"
+    val HEIGHT_1 = Height(imperial = "9 - 11.5", metric = "23 - 29")
+    val WEIGHT_1 = Weight(imperial = "6 - 13", metric = "3 - 6")
+
+    val NAME_2 = "Afghan Hound"
+    val ID_2 = 2
+    val BRED_FOR_2 = "Coursing and hunting"
+    val TEMPERAMENT_2 = "Aloof, Clownish, Dignified, Independent, Happy"
+    val LIFE_SPAN_2 = "10 - 13 years"
+    val HEIGHT_2 = Height(imperial = "25 - 27", metric = "64 - 69")
+    val WEIGHT_2 = Weight(imperial = "50 - 60", metric = "23 - 27")
+
+    @Test
+    fun test_BreedResponseItemsAreEqual(){
+        var breed1 = CurrentBreedResponseItem(BRED_FOR_1, HEIGHT_1, ID_1, LIFE_SPAN_1, NAME_1, TEMPERAMENT_1, WEIGHT_1)
+        var breed2 = CurrentBreedResponseItem(BRED_FOR_1, HEIGHT_1, ID_1, LIFE_SPAN_1, NAME_1, TEMPERAMENT_1, WEIGHT_1)
+        assertEquals(breed1,breed2)
+        println("both breeds are equal!")
+    }
+
+    @Test
+    fun test_BreedNamesAreDifferent() {
+        var breed1 = CurrentBreedResponseItem(BRED_FOR_1, HEIGHT_1, ID_1, LIFE_SPAN_1, NAME_1, TEMPERAMENT_1, WEIGHT_1)
+        var breed2 = CurrentBreedResponseItem(BRED_FOR_1, HEIGHT_1, ID_1, LIFE_SPAN_1, NAME_2, TEMPERAMENT_1, WEIGHT_1)
+        Assert.assertNotEquals(breed1,breed2)
+        println("both breeds are as expected not equal!")
+    }
+
+    @Test
+    fun test_BreedIdsAreDifferent() {
+        var breed1 = CurrentBreedResponseItem(BRED_FOR_1, HEIGHT_1, ID_1, LIFE_SPAN_1, NAME_1, TEMPERAMENT_1, WEIGHT_1)
+        var breed2 = CurrentBreedResponseItem(BRED_FOR_1, HEIGHT_1, ID_2, LIFE_SPAN_1, NAME_1, TEMPERAMENT_1, WEIGHT_1)
+        Assert.assertNotEquals(breed1,breed2)
+        println("both breeds are as expected not equal!")
+    }
+
+    @Test
+    fun test_BreedBredForsAreDifferent() {
+        var breed1 = CurrentBreedResponseItem(BRED_FOR_1, HEIGHT_1, ID_1, LIFE_SPAN_1, NAME_1, TEMPERAMENT_1, WEIGHT_1)
+        var breed2 = CurrentBreedResponseItem(BRED_FOR_2, HEIGHT_1, ID_1, LIFE_SPAN_1, NAME_1, TEMPERAMENT_1, WEIGHT_1)
+        Assert.assertNotEquals(breed1,breed2)
+        println("both breeds are as expected not equal!")
+    }
+
+    @Test
+    fun test_BreedTemperamentsAreDifferent() {
+        var breed1 = CurrentBreedResponseItem(BRED_FOR_1, HEIGHT_1, ID_1, LIFE_SPAN_1, NAME_1, TEMPERAMENT_1, WEIGHT_1)
+        var breed2 = CurrentBreedResponseItem(BRED_FOR_1, HEIGHT_1, ID_1, LIFE_SPAN_1, NAME_1, TEMPERAMENT_2, WEIGHT_1)
+        Assert.assertNotEquals(breed1,breed2)
+        println("both breeds are as expected not equal!")
+    }
+
+    @Test
+    fun test_BreedLifeSpansAreDifferent() {
+        var breed1 = CurrentBreedResponseItem(BRED_FOR_1, HEIGHT_1, ID_1, LIFE_SPAN_1, NAME_1, TEMPERAMENT_1, WEIGHT_1)
+        var breed2 = CurrentBreedResponseItem(BRED_FOR_1, HEIGHT_1, ID_1, LIFE_SPAN_2, NAME_1, TEMPERAMENT_1, WEIGHT_1)
+        Assert.assertNotEquals(breed1,breed2)
+        println("both breeds are as expected not equal!")
+    }
+
+    @Test
+    fun test_BreedHeightsAreDifferent() {
+        var breed1 = CurrentBreedResponseItem(BRED_FOR_1, HEIGHT_1, ID_1, LIFE_SPAN_1, NAME_1, TEMPERAMENT_1, WEIGHT_1)
+        var breed2 = CurrentBreedResponseItem(BRED_FOR_1, HEIGHT_2, ID_1, LIFE_SPAN_1, NAME_1, TEMPERAMENT_1, WEIGHT_1)
+        Assert.assertNotEquals(breed1,breed2)
+        println("both breeds are as expected not equal!")
+    }
+
+    @Test
+    fun test_BreedWeightsAreDifferent() {
+        var breed1 = CurrentBreedResponseItem(BRED_FOR_1, HEIGHT_1, ID_1, LIFE_SPAN_1, NAME_1, TEMPERAMENT_1, WEIGHT_1)
+        var breed2 = CurrentBreedResponseItem(BRED_FOR_1, HEIGHT_1, ID_1, LIFE_SPAN_1, NAME_1, TEMPERAMENT_1, WEIGHT_2)
+        Assert.assertNotEquals(breed1,breed2)
+        println("both breeds are as expected not equal!")
+    }
+}
---
+++ app/src/test/java/Dog_Breeds/data/network/response/HeightTest.kt
@@ -0,0 +1,31 @@
+package Dog_Breeds.data.network.response
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+internal class HeightTest {
+
+    val HEIGHT = Height(imperial = "9 - 11.5", metric = "22.86 - 29.21")
+
+    var heightImperialList = HEIGHT.imperial?.split("-")
+    var heightMetricList = HEIGHT.metric?.split("-")
+
+    var imperialHeightMin = heightImperialList?.get(0)?.toDouble()
+    var imperialHeightMax = heightImperialList?.get(1)?.toDouble()
+
+    var metricHeightMin = heightMetricList?.get(0)?.toDouble()
+    var metricHeightMax = heightMetricList?.get(1)?.toDouble()
+
+
+    @Test
+    fun test_convertAndCheckAllHeights(){
+        InchesToCentimeterConverter (imperialHeightMin,metricHeightMin)
+        InchesToCentimeterConverter (imperialHeightMax, metricHeightMax)
+        println("success!")
+    }
+
+    fun InchesToCentimeterConverter (imperial : Double?, metric : Double?){
+        var imperialInMetric = (imperial?.times(2.54))
+        assertEquals(imperialInMetric, metric)
+    }
+}
